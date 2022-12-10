021174dd4a569eebed0471b228378fff7087287f
Chijioke Ibekwe <chijioke.ibekwe@sabiroad.com>
2022-12-10 23:28:03 +0100

    Update navbar on pages

--- src/main/java/com/project/pingme/config/AwsCloudConfig.java
+++ src/main/java/com/project/pingme/config/AwsCloudConfig.java
@@ -1,7 +1,13 @@
 package com.project.pingme.config;
 
+import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSAsyncClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -13,5 +19,13 @@ public class AwsCloudConfig {
     @Primary
     public AWSCredentialsProvider defaultAwsCredentialsProvider(){
         return new DefaultAWSCredentialsProviderChain();
+    }
+
+    @Bean
+    public AmazonS3 amazonS3(AWSCredentialsProvider awsCredentialsProvider)
+    {
+        return AmazonS3Client.builder()
+                .withCredentials(awsCredentialsProvider)
+                .build();
     }
 }
