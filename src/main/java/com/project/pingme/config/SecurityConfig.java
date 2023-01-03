803a110ebb1b21d273f3c9e508ca700dda68f41f
Chijioke Ibekwe <chijioke.ibekwe@sabiroad.com>
2023-01-03 21:10:32 +0100

    Use layout and fragments on login and signup pages

--- src/main/java/com/project/pingme/config/SecurityConfig.java
+++ src/main/java/com/project/pingme/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/user/signup", "/css/**", "/js/**").permitAll()
+                .antMatchers("/", "/user/signup", "/css/**", "/js/**", "/webjars/**").permitAll()
                 .anyRequest().authenticated();
 
         http.formLogin()
