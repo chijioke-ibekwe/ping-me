967bdc5c1d1b7e9b0be32643f139faed4bb9fbbb
Chijioke Ibekwe <chijioke.ibekwe@sabiroad.com>
2023-01-20 20:17:20 +0100

    Refactor: Remove redundant code from script.js file

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -244,12 +244,6 @@ function activateNavLink(nav){
 }
 
 function processImageUpload(){
-//    const dataDiv = document.querySelector("#data-div");
-//    document.querySelector("#firstName").value = dataDiv.getAttribute('data-first-name');
-//    document.querySelector("#lastName").value = dataDiv.getAttribute('data-last-name');
-//    document.querySelector("#username").value = dataDiv.getAttribute('data-user-name');
-//    document.querySelector("#phoneNumber").value = dataDiv.getAttribute('data-phone-number');
-
     const image_upload_element = document.querySelector("#file-upload");
 
     image_upload_element.addEventListener("change", function(e){
